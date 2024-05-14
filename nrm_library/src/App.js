87f4d3dedb4bb77b03f96af9c4a4c39ff5f4a6c8
leunghon <leunghon5@gmail.com>
2024-05-14 15:52:47 -0700

    initial commit

--- nrm_library/src/App.js
+++ nrm_library/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BookList from './components/BookList';
+import BookDetails from './components/BookDetails';
+import SearchBar from './components/SearchBar';
+import AdminDashboard from './admin/AdminDashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <SearchBar />
+                <Switch>
+                    <Route path="/" exact component={BookList} />
+                    <Route path="/books/:id" component={BookDetails} />
+                    <Route path="/admin" component={AdminDashboard} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ nrm_library/src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+function SearchBar() {
+    return (
+        <TextField fullWidth label="Search Books" variant="outlined" />
+    );
+}
+
+export default SearchBar;
