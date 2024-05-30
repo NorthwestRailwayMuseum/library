b1b635a635e5a214a948a1e62bde3e28e039a3ce
leunghon <leunghon5@gmail.com>
2024-05-29 20:05:25 -0700

    changes

--- nrm_library/src/api.js
+++ nrm_library/src/api.js
@@ -1,14 +1,25 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.API_URL; // Your backend URL
+const API_URL = process.env.REACT_APP_API_URL; // Your backend URL
+
 
 export const getBooks = async () => {
-  const response = await axios.get(`${API_URL}/books/`);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_URL}/books/`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    throw error;
+  }
 };
 
 export const createBook = async (book) => {
-  const response = await axios.post(`${API_URL}/books/`, book);
-  return response.data;
+  try {
+    const response = await axios.post(`${API_URL}/books/`, book);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating book:", error);
+    throw error;
+  }
 };
